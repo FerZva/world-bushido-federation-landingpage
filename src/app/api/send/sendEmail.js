386ad8f0cb0d36e7b7d1bc5b33e3a38128ff1102
FerZva <fernandzva2019@outlook.es>
2024-04-10 22:21:42 -0600

    files fixed

---
+++ src/app/api/send/sendEmail.js
@@ -0,0 +1,9 @@
+// "use server";
+
+// import { Resend } from 'resend'
+
+// export const SendEmail = async(formData) => {
+//     console.log("Running on server");
+//     console.log(formData.get("SenderEmail"));
+//     console.log(formData.get("Message"));
+// }

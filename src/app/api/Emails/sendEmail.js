91cbe62552f854f48dc0a80041ace3e60f33cd33
FerZva <fernandzva2019@outlook.es>
2024-04-10 22:17:49 -0600

    removing corrupted file

--- src/app/api/Emails/sendEmail.js
+++
@@ -1,9 +0,0 @@
-// "use server";
-
-// import { Resend } from 'resend'
-
-// export const SendEmail = async(formData) => {
-//     console.log("Running on server");
-//     console.log(formData.get("SenderEmail"));
-//     console.log(formData.get("Message"));
-// }
